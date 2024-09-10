56f3d893b619811057808b38754231e666aeb632
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-09-10 16:00:08 +0900

    [level 0] Title: 가까운 수, Time: 1.66 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -1,13 +1,19 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] array, int n) {
+        // [3, 10, 28]
         int answer = 0;
+        Arrays.sort(array);
         
-        for(int i = 0; i < array.length; i++){
-            int temp = n - array[i];
-            System.out.println(temp);
+        for(int i = 1; i < array.length; i++) {
+            if(Math.abs(n-array[0]) > Math.abs(n-array[i])) {
+                array[0] = array[i];
+            }            
         }
+        answer = array[0]; 
+        
         return answer;
-        
-        
+
     }
 }
