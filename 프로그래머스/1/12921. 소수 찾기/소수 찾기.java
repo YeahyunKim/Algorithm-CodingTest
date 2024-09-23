20f0169689497983c9ac9438ba9789b8286d0da2
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-09-23 17:41:17 +0900

    [level 1] Title: 소수 찾기, Time: 268.57 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,20 +1,23 @@
+import java.util.*;
 class Solution {
-  public int solution(int n) {
-      int answer = 0;
-            
-      for(int i = 2; i <= n; i++){
-          boolean chk = true;
-          for(int j = 2; j*j <= i; j++){ //배수 지우기
-              if(i % j == 0){
-                  chk = false;
-                  break;
-              }
-          }
-          if(chk){
-              answer++;
-          }
-      }
-      
-     return answer;           
-  }
+    public int solution(int n) {
+        int answer = 0;
+        boolean divisor = true;
+
+        for(int i = 2; i <= n; i++) {
+            divisor = true;
+            for(int j = 2; j <= Math.sqrt(i); j++) {
+                if(i % j == 0) {
+                    divisor = false;
+                    break;
+                }
+            }
+            if(divisor) {
+                answer++;
+            }
+        }
+        
+    
+        return answer;
+    }
 }
