fa082109498c36e0860cff8f27da3a8a21416f2f
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-10-03 16:28:16 +0900

    [level 2] Title: 숫자의 표현, Time: 1.83 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -2,10 +2,22 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        for(int i = 1; i <= n; i+=2) {
-            if(n % i == 0) answer++;
-        }
+        // for(int i = 1; i <= n; i+=2) {
+        //     if(n % i == 0) answer++;
+        // }
         
+        for(int i = 1; i <= n; i++) {
+            int sum = 0;
+            for(int j = i; j <= n; j++) {
+                sum += j;
+                if(sum == n) {
+                    answer++;
+                    break;
+                } else if (sum > n) {
+                    break;
+                }
+            }
+        }
         return answer;
     }
 }
