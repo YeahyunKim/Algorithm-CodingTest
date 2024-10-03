15c3978d48d403fa62b18ace81b10540b9dae552
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-10-04 07:30:54 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 27.73 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,41 +1,30 @@
 class Solution {
     public int[] solution(String s) {
         int[] answer = {0, 0};
-        int changeStr = 0;
-        String binaryStr = "";
-        int zeroCount = 0;
-        int binaryCount = 0;
-        String newStr = "";
+        int temp = 0;
 
         while(!s.equals("1")) {
-            changeStr = s.length();
-            if(s.contains("0")) {
-                for(int i = 0; i < s.length(); i++) {
-                    if (s.charAt(i) == '0') {
-                        zeroCount++;
-                    } else {
-                        newStr += s.charAt(i);
-                    }
-                }
-                s = newStr;
-                newStr = "";
-            } else {
-                while(changeStr > 0) {
-                    if(changeStr % 2 == 1) {
-                        binaryStr = "1" + binaryStr;
-                    } else {
-                        binaryStr += "0";
-                    }
-                    changeStr = changeStr / 2;
-                }
-                binaryCount++;
-                s = binaryStr;
-                binaryStr = "";
-            }
-
+            //전체 길이 구하기
+            temp = s.length();
+            //2
+            //2
+            
+            //0 제거
+            s = s.replaceAll("0", "");
+            //없음
+            //10 -> 1
+            
+            //전체길이에서 제거한 0의 개수 더하기
+            answer[1] += temp - s.length();
+            //1 = 2 - 1 //현재 3
+            
+            //제거된 s의 길이를 BianryString으로 바꾸기
+            s = Integer.toBinaryString(s.length());
+            //1을 바꾸면, 1
+            
+            //변환결과 개수 1더하기  //현재 3
+            answer[0]++;   
         }
-        answer[0] = binaryCount + 1;
-        answer[1] = zeroCount;
         return answer;
     }
 }
