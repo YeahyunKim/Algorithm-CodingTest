e7b8e573b649df407a0852b211c555e40e3b37e6
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-10-11 17:58:07 +0900

    [level 2] Title: 괄호 회전하기, Time: 432.57 ms, Memory: 400 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,62 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int x = 1;
+        
+        Queue<Character> circularQ = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+        int xLength = s.length();
+        
+            
+        
+        while(x <= s.length()) {
+            
+            for(char c : s.toCharArray()) {
+                if(stack.isEmpty()) {
+                    stack.push(c);   
+                } else {
+                    if (stack.peek() == '[' && c == ']') {
+                        stack.pop();
+                    }
+                    else if (stack.peek() == '{' && c == '}') {
+                        stack.pop();
+                    }
+                    else if (stack.peek() == '(' && c == ')') {
+                        stack.pop();
+                    }
+                    else {
+                        stack.push(c);
+                    }
+                }
+            }
+            
+            
+            if(stack.size() == 0) {
+                answer++;
+            }
+            
+            
+            for(char c : s.toCharArray()) { 
+                circularQ.offer(c);
+            }
+
+            
+            circularQ.add(circularQ.poll());
+
+            
+            s = ""; 
+            
+            for(int i = 0; i < xLength; i++) {
+                s += circularQ.poll();
+            }
+            
+            stack.clear();
+
+            x++;
+        }
+        
+    
+        return answer;
+    }
+}
