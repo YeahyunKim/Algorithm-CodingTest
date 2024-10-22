d1ac5d802b9c5e34b9aa5bfbf170fecc27da5175
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-10-22 15:10:59 +0900

    [level 2] Title: 롤케이크 자르기, Time: 360.23 ms, Memory: 170 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -6,35 +6,23 @@ class Solution {
         HashMap<Integer, Integer> hashMap1 = new HashMap<>();
         HashMap<Integer, Integer> hashMap2 = new HashMap<>();
         
-        // 전체 토핑의 수를 hashMap2에 저장
-        for (int t : topping) {
-            hashMap2.put(t, hashMap2.getOrDefault(t, 0) + 1);
+        for(int i = 0; i < topping.length; i++) {
+            hashMap2.put(topping[i], hashMap2.getOrDefault(topping[i], 0) + 1);
         }
-
-        int uniqueCount1 = 0; // 첫 번째 부분의 고유한 토핑 수
-        int uniqueCount2 = hashMap2.size(); // 두 번째 부분의 고유한 토핑 수
-
-        for (int i = 0; i < topping.length; i++) {
-            // 첫 번째 부분에 토핑 추가
-            int currentTopping = topping[i];
-            hashMap1.put(currentTopping, hashMap1.getOrDefault(currentTopping, 0) + 1);
-            if (hashMap1.get(currentTopping) == 1) {
-                uniqueCount1++;
+        
+        for(int i = 0; i < topping.length; i++) {
+            hashMap1.put(topping[i], hashMap1.getOrDefault(topping[i], 0) + 1);
+            
+            hashMap2.put(topping[i], hashMap2.getOrDefault(topping[i], 0) - 1);
+                
+            if(hashMap2.get(topping[i]) == 0) {
+                hashMap2.remove(topping[i]);
             }
-
-            // 두 번째 부분에서 토핑 제거
-            if (hashMap2.get(currentTopping) != null) {
-                hashMap2.put(currentTopping, hashMap2.get(currentTopping) - 1);
-                if (hashMap2.get(currentTopping) == 0) {
-                    hashMap2.remove(currentTopping);
-                    uniqueCount2--;
-                }
-            }
-
-            // 고유한 토핑 수가 같으면 카운트 증가
-            if (uniqueCount1 == uniqueCount2) {
+            
+            if(hashMap1.size() == hashMap2.size()) {
                 answer++;
             }
+                
         }
 
         return answer;
