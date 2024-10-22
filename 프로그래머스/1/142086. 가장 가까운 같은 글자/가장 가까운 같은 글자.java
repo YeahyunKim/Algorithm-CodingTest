391cdce2b7aa66f40b82c462f67ce6d0335ff5b6
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-10-22 10:52:53 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 5.13 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,18 +1,19 @@
+import java.util.*;
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
+        HashMap<Character, Integer> hashMap = new HashMap<>();
         
-        for (int i = 0; i < s.length(); i++) {
-			for (int j = i; j >= 0; j--) {
-				if (s.charAt(i) == s.charAt(j) && answer[i] == 0) {
-					answer[i] = i - j;
-					
-				}
-			}
-			if (answer[i] == 0) {
-				answer[i] = -1;
-			}
-		}
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(hashMap.get(s.charAt(i)) == null){
+                hashMap.put(s.charAt(i), i);
+                answer[i] = -1;
+            } else {
+                answer[i] = i - hashMap.get(s.charAt(i));
+                hashMap.put(s.charAt(i), i);
+            }
+        }
         
         return answer;
     }
