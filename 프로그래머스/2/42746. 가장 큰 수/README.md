af947c801f2635c03661086b3c1d54244d05e48a
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-10-23 11:38:43 +0900

    [level 2] Title: 가장 큰 수, Time: 172.29 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 126 MB, 시간: 322.34 ms
+메모리: 131 MB, 시간: 172.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 23일 10:49:36
+2024년 10월 23일 11:38:41
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,20 +1,32 @@
 import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-        
-        String[] strList = new String[numbers.length];
-        
-        for(int i = 0; i < numbers.length; i++) {
-            strList[i] = numbers[i] + "";
+        // 정수를 문자열로 변환
+        String[] strNumbers = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            strNumbers[i] = String.valueOf(numbers[i]);
         }
-        
-        Arrays.sort(strList, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
-        
-        if(strList[0].equals("0")) {
+
+        // 정렬 기준 정의
+        Arrays.sort(strNumbers, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                // o1 + o2와 o2 + o1을 비교
+                return (o2 + o1).compareTo(o1 + o2);
+            }
+        });
+
+        // 정렬된 문자열 배열을 이어 붙임
+        StringBuilder result = new StringBuilder();
+        for (String str : strNumbers) {
+            result.append(str);
+        }
+
+        // 결과가 "0"인 경우를 처리
+        if (result.charAt(0) == '0') {
             return "0";
         }
-        
-        return String.join("", strList);
+
+        return result.toString();
     }
 }
