8b36063aafd0c902b9429944e0e226b5cac4b2c9
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-10-24 17:21:11 +0900

    [level 2] Title: 롤케이크 자르기, Time: 241.78 ms, Memory: 172 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -7,23 +7,21 @@ class Solution {
         HashMap<Integer, Integer> hashMap2 = new HashMap<>();
         
         for(int i = 0; i < topping.length; i++) {
-            hashMap2.put(topping[i], hashMap2.getOrDefault(topping[i], 0) + 1);
+            hashMap1.put(topping[i], hashMap1.getOrDefault(topping[i], 0) + 1);
         }
         
         for(int i = 0; i < topping.length; i++) {
-            hashMap1.put(topping[i], hashMap1.getOrDefault(topping[i], 0) + 1);
+            hashMap2.put(topping[i], hashMap2.getOrDefault(topping[i], 0) + 1);
+            hashMap1.put(topping[i], hashMap1.getOrDefault(topping[i], 0) - 1);
             
-            hashMap2.put(topping[i], hashMap2.getOrDefault(topping[i], 0) - 1);
-                
-            if(hashMap2.get(topping[i]) == 0) {
-                hashMap2.remove(topping[i]);
+            if(hashMap1.get(topping[i]) == 0) {
+                hashMap1.remove(topping[i]);
             }
             
-            if(hashMap1.size() == hashMap2.size()) {
-                answer++;
-            }
-                
+            if(hashMap1.size() == hashMap2.size()) answer++;
+
         }
+        
 
         return answer;
     }
