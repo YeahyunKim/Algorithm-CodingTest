ed6513ec0e640eb38e9301313110fad97a22944a
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-10-24 11:06:49 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 82.29 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,45 +2,23 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-            String answer = "";
-            int index = 1;
-//         ArrayList<String> list = new ArrayList<>();
-//         ArrayList<String> goalList = new ArrayList<>();
-        
-//         for(String s : participant){
-//             list.add(s);
-//         }
-        
-//         for(String s : completion) {
-//             goalList.add(s);
-//         }
-        
-//         for(String goal : goalList) {
-//             list.remove(goal);
-//         }
-        
-//         String answer = String.join(", ", list);
-        
-        
-        HashMap<String, Integer> hashMap = new HashMap();
+        String answer = "";
+        HashMap<String, Integer> hashMap = new HashMap<>();
         
         for(String s : participant) {
-            hashMap.put(s, hashMap.getOrDefault(s, 0) + 1);
-        }
-
-        for(String s : completion) {
-            hashMap.put(s, hashMap.get(s) -1);
+            hashMap.put(s, hashMap.getOrDefault(s, 0) + 1);   
         }
         
-        for(String s : hashMap.keySet()) {
-            if(hashMap.get(s) > 0) {
-                answer = s;
+        for(String s : completion) {
+            hashMap.put(s, hashMap.getOrDefault(s, 0) - 1);
+        }
+        
+        for(Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+            if(entry.getValue() != 0) {
+                answer = entry.getKey();
             }
         }
-        
-        // for(Map.Entry<String, Integer> entry : hashMap.entrySet()) {
-        //  System.out.println(entry.getKey() + ", " + entry.getValue());   
-        // }
+
         
         return answer;
     }
