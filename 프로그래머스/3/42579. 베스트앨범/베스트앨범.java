fc4f7fc1f16fee1afc50f044454ee8b6342e52a2
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-10-25 16:39:04 +0900

    [level 3] Title: 베스트앨범, Time: 2.28 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,57 +2,41 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        // HashMap<Integer, Integer> uniqueNumMap = new HashMap<>();
-        
+        HashMap<String, Integer> hashMap1 = new HashMap<>();
         HashMap<String, HashMap<Integer, Integer>> map = new HashMap<>();
         
         ArrayList<Integer> answerList = new ArrayList<>();
         
-        for(int i = 0; i < genres.length; i++) {
-            hashMap.put(genres[i], hashMap.getOrDefault(genres[i], 0) + plays[i]);
-            // uniqueNumMap.put(i, uniqueNumMap.getOrDefault(i, plays[i]));
-            
-            //map.put(genres[i], map.getOrDefault(genres[i], new HashMap<Integer, Integer>()).put(i, plays[i]));
-            HashMap<Integer, Integer> innerMap = map.getOrDefault(genres[i], new HashMap<Integer, Integer>());
-            // HashMap<Integer, Integer> innerMap = new HashMap<>();
+        for(int i = 0; i < genres.length; i++) {    
+            hashMap1.put(genres[i], hashMap1.getOrDefault(genres[i], 0) + plays[i]);
+            HashMap<Integer, Integer> innerMap = map.getOrDefault(genres[i], new HashMap<>());
             innerMap.put(i, plays[i]);
+            
             map.put(genres[i], innerMap);
         }
         
+        ArrayList<String> arrayList = new ArrayList<>(hashMap1.keySet());
         
-        // System.out.println("map = " + map);
+        Collections.sort(arrayList, (o1, o2) -> Integer.compare(hashMap1.get(o2), hashMap1.get(o1)));
         
-        // System.out.println("장르별 총 점수 = " + hashMap);
-        // System.out.println("고유번호별 점수 = " + uniqueNumMap);
-        
-        ArrayList<String> list = new ArrayList<>(hashMap.keySet());
-        
-        Collections.sort(list, (o1, o2) -> Integer.compare(hashMap.get(o2), hashMap.get(o1)));
-        
-        // System.out.println("장르별 랭킹 = " + list.toString());
-        
-        for(String s : list) {
-            ArrayList<Integer> result = new ArrayList<>(map.get(s).keySet());
+        for(String s : arrayList) {
+            ArrayList<Integer> rank = new ArrayList<>(map.get(s).keySet());
             
-            Collections.sort(result, (o1, o2) -> Integer.compare(map.get(s).get(o2), map.get(s).get(o1)));
+            Collections.sort(rank, (o1, o2) -> Integer.compare(map.get(s).get(o2), map.get(s).get(o1)));
             
-            
-            if(result.size() >= 2) {
-                answerList.add(result.get(0));
-                answerList.add(result.get(1));
+            if(rank.size() >= 2) {
+                answerList.add(rank.get(0));
+                answerList.add(rank.get(1));
             } else {
-                answerList.add(result.get(0));
+                answerList.add(rank.get(0));
             }
-            
-            
         }
         
         int[] answer = new int[answerList.size()];
-        for(int i = 0; i < answer.length; i++) {
+        
+        for(int i = 0; i < answerList.size(); i++) {
             answer[i] = answerList.get(i);
         }
-        
         return answer;
     }
 }
