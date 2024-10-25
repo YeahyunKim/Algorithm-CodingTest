267fd06aa0fec5cb5d0e211ac92cd72212cb8152
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-10-25 17:27:22 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.15 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,27 +1,21 @@
 import java.util.*;
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        char[] charArr = s.toCharArray();
-        
-        boolean isFirst = true;
-        
-        for(int i = 0; i < charArr.length; i++) {
-            if(isFirst) {
-                answer += Character.toUpperCase(charArr[i]);
-                if (charArr[i] == ' ') {
-                    isFirst = true;    
-                } else {
-                    isFirst = false;
-                }
-            } else if (charArr[i] == ' '){
-                answer += charArr[i];
-                isFirst = true;
+        StringBuilder answer = new StringBuilder();
+        boolean isFirstWord = true;
+        for(char c : s.toCharArray()) {
+            if(isFirstWord && c != ' ') {
+                answer.append(Character.toUpperCase(c));
+                isFirstWord = false;
             } else {
-                answer += Character.toLowerCase(charArr[i]);
+                if (c == ' ') { 
+                    answer.append(c);
+                    isFirstWord = true;
+                } else {
+                    answer.append(Character.toLowerCase(c));
+                }
             }
         }
-        
-        return answer;
+        return answer.toString();
     }
 }
