1b5373fb3ba7f7e3736a3042f22dfea380a952e4
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-10-25 17:50:17 +0900

    [level 2] Title: 영어 끝말잇기, Time: 9.00 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -3,48 +3,29 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
-
-        
         ArrayList<String> wordList = new ArrayList<>();
-
-        String[] arrStr = words[0].split("");
-        String lastWord = arrStr[arrStr.length - 1]; // K
         
         for(int i = 0; i < words.length; i++) {
             
-            
-            String[] wordArr = words[i].split("");
-            String firstWord = wordArr[0]; // K
-            
-            if(wordList.size() == 0) {
-                wordList.add(words[i]);
-            } else {
-            
-            
-                if (wordList.contains(words[i])) {
+            //처음 플레이는 페스
+            if(i != 0) {
+                String firstWord = words[i].charAt(0) + "";
+                String lastWord = words[i - 1].charAt(words[i - 1].length() - 1) + "";
+                if(wordList.contains(words[i])) {
+                    answer[0] = (i % n) + 1;
+                    answer[1] = (i / n) + 1;
+                    break;
+                } else if (!firstWord.equals(lastWord)) {
                     answer[0] = (i % n) + 1;
                     answer[1] = (i / n) + 1;
                     break;
                 }
-                else if (!lastWord.equals(firstWord)) {
-                    answer[0] = (i % n) + 1;
-                    answer[1] = (i / n) + 1;
-                    break;
-                } 
-                lastWord = wordArr[wordArr.length - 1];
-                wordList.add(words[i]);
-                
-                
-                System.out.println(lastWord + ", " + firstWord);
             }
-              
-   
-
-
-                // if ()
-    
+            wordList.add(words[i]);
+            
+            
         }
-
+        
         return answer;
     }
 }
