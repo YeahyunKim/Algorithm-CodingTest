dc5f4e0bde5fd504f97e6d0dcfb993295fcdb775
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-10-31 17:43:27 +0900

    [Bronze V] Title: 검증수, Time: 180 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+
+        for(int i = 0; i < 5; i++) {
+            int num = input.nextInt();
+            sum += num * num;
+        }
+        
+        System.out.println(sum % 10);
+        
+    }
+    
+
+}
