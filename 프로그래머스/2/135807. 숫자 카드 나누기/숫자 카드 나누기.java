fb09b810470cdbd3b28b72f94d045bac9c02436c
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-11-04 10:52:19 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 24.12 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -36,6 +36,12 @@ class Solution {
         return answer;
     }
     
+    
+    
+    
+    
+    
+    
     public static int gcd(int num1, int num2) {
         if(num1 % num2 == 0) {
             return num2;
