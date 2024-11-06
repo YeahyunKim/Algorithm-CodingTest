490db828ea1df7a55f8f7796538722bcdb8f7d79
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-11-06 11:50:26 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 2288 ms, Memory: 333136 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class Main {
+    static int N, M, R;
+    static int[] answer;
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int order = 1;
+    
+    public static void dfs(int idx) {
+        visited[idx] = true;
+        answer[idx] = order;
+        order++;
+        
+        for(int i = 0; i < graph[idx].size(); i++) {
+            if(!visited[graph[idx].get(i)])
+                dfs(graph[idx].get(i));
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        N = input.nextInt();
+        M = input.nextInt();
+        R = input.nextInt();
+        
+        answer = new int[N+1];
+        visited = new boolean[N+1];
+        graph = new ArrayList[N+1];
+        
+        for(int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for(int i = 0; i < M; i++) {
+            int x = input.nextInt();
+            int y = input.nextInt();
+
+            graph[x].add(y);
+            graph[y].add(x);
+        }
+        
+        for(int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+        
+        dfs(R);
+        
+        for(int i = 1; i <= N; i++) {
+            System.out.println(answer[i]);
+        }
+        
+    }
+}
