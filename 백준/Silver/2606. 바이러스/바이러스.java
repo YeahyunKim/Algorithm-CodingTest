38bc22e408d55973da9a10459d2f0b6275dbe925
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-11-06 14:49:52 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,45 +1,51 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
-    static int N,M;
-    static boolean[][] graph;
+public class Main {
+    static int N, M;
+    static int answer = 0;
     static boolean[] visited;
-    static int answer;    
-
+    static ArrayList<Integer>[] graph;
+    
     public static void dfs(int idx) {
         visited[idx] = true;
         answer++;
-         
-        for(int i = 1; i <= N; i++) {
-            if(!visited[i] && graph[idx][i]) {
-                dfs(i);
+        for(int i = 0; i < graph[idx].size(); i++) {
+            if(!visited[graph[idx].get(i)]) {
+
+                dfs(graph[idx].get(i));   
             }
         }
+
     }
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-
-        graph = new boolean[N+1][N+1];
-        visited = new boolean[N+1];
         
-        int x, y;
-        for(int i = 0; i < M; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            x = Integer.parseInt(st.nextToken());
-            y = Integer.parseInt(st.nextToken());
+        visited = new boolean[N+1];
+        graph = new ArrayList[N+1];
 
-            graph[x][y] = true;
-            graph[y][x] = true;
+        for(int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();            
         }
-
+        
+        for(int i = 0; i < M; i++) {
+             StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            
+            graph[x].add(y);
+            graph[y].add(x);
+        }
+        
         dfs(1);
-
+        
         System.out.println(answer - 1);
+        
+
     }
 }
