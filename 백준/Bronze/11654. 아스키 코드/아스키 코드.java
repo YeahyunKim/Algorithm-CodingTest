369cdd6e07ee6b0a5baee6cbf442085d6186f1a0
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-11-07 16:15:33 +0900

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int c = input.next().charAt(0);
+        
+        System.out.println(c);
+    }
+}
