cd5b347ac2ca4e5f09a10eefa44c8d887d8a09fd
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-11-19 14:28:33 +0900

    [level 3] Title: 네트워크, Time: 1.05 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,38 +1,43 @@
 import java.util.*;
 class Solution {
-    static boolean[][] graph;
+    static ArrayList<Integer>[] graph;
     static boolean[] visited;
-
-    public static void dfs(int n, int idx) {
+    static int answer = 0;
+    
+    public static void dfs(int cpu, int idx) {
         visited[idx] = true;
         
-        for(int i = 1; i <= n; i++) {
-            if(!visited[i] && graph[idx][i]) {
-                 dfs(n, i);
-            }   
+        for(int i = 0; i < graph[idx].size(); i++) {
+            if(!visited[graph[idx].get(i)]) {
+                dfs(cpu, graph[idx].get(i));
+                
+            }
         }
     }
     
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        graph = new boolean[n+1][n+1];
-        visited = new boolean[n+1];
+        
+        graph = new ArrayList[computers.length + 1];
+        visited = new boolean[computers.length + 1];
+        
+        for(int i = 1; i <= computers.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
         
         for(int i = 0; i < computers.length; i++) {
             for(int j = 0; j < computers[i].length; j++) {
                 if(i != j) {
-                    if(computers[i][j] == 1){
-                        // System.out.println(i + " / " + j);
-                    
-                        graph[i+1][j+1] = true;
-                        graph[j+1][i+1] = true;
-
-                    }
+                    if(computers[i][j] == 1 && !graph[i+1].contains(j+1)) {
+                        graph[i+1].add(j+1);
+                        graph[j+1].add(i+1);
+                    }    
                 }
                 
             }
         }
         
+        
+        // System.out.println(Arrays.toString(visited));
         
         for(int i = 1; i <= n; i++) {
             if(!visited[i]) {
@@ -40,7 +45,7 @@ class Solution {
                 answer++;
             }
         }
-        
         return answer;
+
     }
 }
