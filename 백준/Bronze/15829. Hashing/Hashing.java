1a76cecd4228fd40992d5c1dfa2fceed08747f64
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-12-10 10:39:31 +0900

    [Bronze II] Title: Hashing, Time: 184 ms, Memory: 17896 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,18 +1,20 @@
+import java.math.BigInteger;
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        long answer = 0;
+        BigInteger answer = BigInteger.valueOf(0);
         int N = input.nextInt();
         input.nextLine();
         String[] s = input.nextLine().split("");
         for(int i = 0; i < N; i++) {
             int a = s[i].charAt(0) - 96;
-            answer += (long) (a * Math.pow(31, i));
+            BigInteger term = BigInteger.valueOf(a).multiply(BigInteger.valueOf(31).pow(i));
+            answer = answer.add(term); // BigInteger의 add 메서드 사용
         }
 
-        System.out.println(answer);
+        System.out.println(answer.remainder(BigInteger.valueOf(1234567891)));
     }
 }
+
 
