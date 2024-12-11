be3e958f35b65423987d443609825c649e1f1b42
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2024-12-11 11:47:27 +0900

    [Silver II] Title: 스택 수열, Time: 940 ms, Memory: 109572 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -14,22 +14,26 @@ public class Main {
             queue.offer(input.nextInt());
         }
 
-for (int i = 1; i <= N; i++) {
-    stack.push(i);
-    sb.append("+").append("\n");
+        for(int i = 1; i <= N; i++) {
+            //1 2 3 4를 넣어줌
+            //[4, 3, 6, 8, 7, 5, 2, 1]
+            if(i <= queue.peek()) {
+                stack.add(i);
+                sb.append("+").append("\n");
+//                System.out.println("+");
+            }
 
-    while (!stack.isEmpty() && !queue.isEmpty() && stack.peek().equals(queue.peek())) {
-        stack.pop();
-        queue.poll();
-        sb.append("-").append("\n");
-    }
-}
+        while (!stack.isEmpty() && !queue.isEmpty() && stack.peek().equals(queue.peek())) {
+                stack.pop();
+                queue.poll();
+                sb.append("-").append("\n");
+            }
+        }
 
-if (!queue.isEmpty()) {
-    System.out.println("NO");
-} else {
-    System.out.println(sb);
-}
-
+        if(!stack.isEmpty() && !queue.isEmpty()) {
+            System.out.println("NO");
+        } else {
+            System.out.println(sb);
+        }
     }
 }
