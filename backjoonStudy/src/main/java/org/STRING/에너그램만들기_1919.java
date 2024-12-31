956db4e64349de429e383ece063bbbece9b9e09a
Yeahyun Kim <insidesy4@gmail.com>
2024-12-31 14:46:35 +0900

    [1919] 애너그램 만들기

--- backjoonStudy/src/main/java/org/STRING/에너그램만들기_1919.java
+++ backjoonStudy/src/main/java/org/STRING/에너그램만들기_1919.java
@@ -1,7 +1,31 @@
 package org.STRING;
+import java.util.Scanner;
 
 public class 에너그램만들기_1919 {
+
+    public static int[] addAlphabetCount(String s) {
+        int[] countStr = new int[26];
+        for(int i = 0; i < s.length(); i++) {
+            countStr[s.charAt(i) - 'a']++;
+        }
+        return countStr;
+    }
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        String str1 = input.next();
+        String str2 = input.next();
+
+        int[] countStr1 = addAlphabetCount(str1);
+        int[] countStr2 = addAlphabetCount(str2);
+
+        int answer = 0;
+
+        for(int i = 0; i < 26; i++) {
+            if(countStr1[i] != countStr2[i]) answer += Math.abs(countStr1[i] - countStr2[i]);
+        }
+
+        System.out.println(answer);
+
     }
 }
