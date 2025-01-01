1d26218712fc01f58f229f4443cf89de19162e23
Yeahyun Kim <insidesy4@gmail.com>
2025-01-01 15:48:14 +0900

    [1302] 베스트 셀러

---
+++ backjoonStudy/src/main/java/org/SORT/베스트셀러_1302.java
@@ -0,0 +1,41 @@
+package org.SORT;
+
+import java.util.*;
+
+public class 베스트셀러_1302 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        int N = input.nextInt();
+
+        for(int i = 0; i < N; i++) {
+            String s = input.next();
+
+            hashMap.put(s, hashMap.getOrDefault(s, 0) + 1);
+        }
+
+        String bookTitle = "";
+        int maxValue = 0;
+        for(Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+            if(maxValue < entry.getValue()) {
+                maxValue = entry.getValue();
+                bookTitle = entry.getKey();
+                arrayList.clear();
+                arrayList.add(bookTitle);
+            }
+            if(maxValue == entry.getValue()) {
+                arrayList.add(bookTitle);
+                arrayList.add(entry.getKey());
+            }
+        }
+
+        if(arrayList.size() > 1) {
+            Collections.sort(arrayList);
+            System.out.println(arrayList.get(0));
+        } else {
+            System.out.println(arrayList.get(0));
+        }
+    }
+}
---
+++ backjoonStudy/src/main/java/org/STRING/소금폭탄_13223.java
@@ -0,0 +1,46 @@
+package org.STRING;
+
+import java.util.Scanner;
+
+public class 소금폭탄_13223 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String[] now = input.next().split(":");
+        String[] later = input.next().split(":");
+        StringBuilder sb = new StringBuilder();
+        int convertNow = 0;
+        int convertLater = 0;
+
+        for(int i = 0; i < now.length; i++) {
+
+            if(i == 0) {
+                convertNow += Integer.parseInt(now[i]) * 60 * 60;
+                convertLater += Integer.parseInt(later[i]) * 60 * 60;
+            }
+
+            if(i == 1) {
+                convertNow += Integer.parseInt(now[i]) * 60;
+                convertLater += Integer.parseInt(later[i]) * 60;
+            }
+
+            if(i == 2) {
+                convertNow += Integer.parseInt(now[i]);
+                convertLater += Integer.parseInt(later[i]);
+            }
+        }
+
+        int convertTime = convertLater - convertNow;
+
+        if(convertLater <= 0) {
+            convertTime += 24 * 60 * 60;
+        }
+
+        int hour = convertTime / 3600;
+        int minutes = (convertTime % 3600) / 60;
+        int seconds = convertTime % 60;
+
+        System.out.printf("%02d:%02d:%02d",hour,minutes,seconds);
+
+    }
+}
