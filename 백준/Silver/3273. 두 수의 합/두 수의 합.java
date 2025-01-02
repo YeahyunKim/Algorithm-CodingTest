3c5530254f176d6fb860716da8ffd24b046bee79
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2025-01-02 16:20:18 +0900

    [Silver III] Title: 두 수의 합, Time: 832 ms, Memory: 102788 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int N = input.nextInt();
+        
+        int[] array = new int[N];
+        
+        for(int i = 0; i < N; i++) {
+            array[i] = input.nextInt();
+        }
+        
+        int X = input.nextInt();
+        
+        Arrays.sort(array);
+        
+        int leftPoint = 0;
+        int rightPoint = N-1;
+        int answer = 0;
+        
+        while(leftPoint < rightPoint) {
+            if(array[leftPoint] + array[rightPoint] == X) {
+                answer++;
+                leftPoint++;
+                rightPoint--;
+            } else if(array[leftPoint] + array[rightPoint] < X) {
+                leftPoint++;
+            } else if(array[leftPoint] + array[rightPoint] > X) {
+                rightPoint--;
+            }
+        }
+        
+        System.out.println(answer);
+    }
+}
