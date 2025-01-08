361f9ffdf2c8af32113f336bb051eb31f653c5ca
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2025-01-08 18:49:43 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 1816 ms, Memory: 311212 KB -BaekjoonHub

---
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+        int M = input.nextInt();
+
+        int[] ground = new int[N+2];
+        int[] groundPrefix = new int[N+2];
+
+        for(int i = 1; i <= N; i++) {
+            int a = input.nextInt();
+            ground[i] = a;
+        }
+
+        for(int i = 0; i < M; i++) {
+            int a = input.nextInt();
+            int b = input.nextInt()+1;
+            int c = input.nextInt();
+
+            groundPrefix[a] += c;
+            groundPrefix[b] -= c;
+        }
+
+
+        for(int i = 1; i <= N; i++) {
+            groundPrefix[i] = groundPrefix[i-1] + groundPrefix[i];
+        }
+        for(int i = 1; i <= N; i++) {
+            ground[i] += groundPrefix[i];
+            System.out.print(ground[i] + " ");
+        }
+    }
+}
