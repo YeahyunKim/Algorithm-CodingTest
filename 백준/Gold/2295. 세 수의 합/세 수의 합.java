1a709106bb1f30a4a089d62977e4878ffac8d105
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2025-01-09 17:19:36 +0900

    [Gold IV] Title: 세 수의 합, Time: 472 ms, Memory: 68720 KB -BaekjoonHub

---
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int N = sc.nextInt();
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++)
+                arr[i] = sc.nextInt();
+
+            Set<Integer> sums = new HashSet<>();
+            for (int i = 0; i < N-1; i++)
+                for (int j = i; j < N; j++)
+                    sums.add(arr[i] + arr[j]);
+
+            int ans = -1;
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++) {
+                    int target = arr[i] - arr[j];
+                    if (sums.contains(target))
+                        ans = Math.max(ans, arr[i]);
+                }
+            System.out.println(ans);
+        }
+
+}
