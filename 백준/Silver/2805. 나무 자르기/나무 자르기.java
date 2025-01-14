aad51439b8ad39300707bb970616bb6298b26c4c
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2025-01-14 11:33:22 +0900

    [Silver II] Title: 나무 자르기, Time: 1652 ms, Memory: 320476 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,45 +1,50 @@
 import java.util.Scanner;
- 
-public class Main {
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		int M = in.nextInt();
-		
-		int[] tree = new int[N];
-		
-		int min = 0;
-		int max = 0;
-		
-		for(int i = 0; i < N; i++) {
-			tree[i] = in.nextInt();
-			if(max < tree[i]) {
-				max = tree[i];
-			}
-		}
 
-		while(min < max) {
-			
-			int mid = (min + max) / 2;
-			long sum = 0;
-			for(int treeHeight : tree) {
-				if(treeHeight - mid > 0) { 
-					sum += (treeHeight - mid);
-				}
-			}
-			
-			if(sum < M) {
-				max = mid;
-			}
-			
-			else {
-				min = mid + 1;
-			}
-		}
-		
-		System.out.println(min - 1);
-		
-	}
+public class Main {
+    static int N, NeedTreeNum;
+    static int[] treeHeight;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        N = input.nextInt();
+        NeedTreeNum = input.nextInt();
+
+        treeHeight = new int[N];
+
+        //나무 개수 넣기
+        for(int i = 0; i < N; i++) {
+            treeHeight[i] = input.nextInt();
+        }
+        System.out.println(getHeight());
+    }
+
+    public static int getHeight() {
+        int l = 0;
+        int r = 1000000000;
+
+        int answer = -1;
+        //한개의 값을 탐색할때는 <=
+        while(l <= r) {
+            int nowHeight = (l + r) / 2;
+
+            if(isPossible(nowHeight)) {
+                l = nowHeight + 1;
+                answer = nowHeight;
+            } else {
+                r = nowHeight - 1;
+            }
+
+        }
+        return answer;
+    }
+
+    public static boolean isPossible(int nowHeight2) {
+        long leftTreeHeight = 0;
+
+        for(int i = 0; i < N; i++) {
+            if(treeHeight[i] > nowHeight2) leftTreeHeight += treeHeight[i] - nowHeight2;
+        }
+
+        return leftTreeHeight >= NeedTreeNum;
+    }
 }
