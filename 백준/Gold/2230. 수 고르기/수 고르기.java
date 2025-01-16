2eff23d1d9e73d7683b6e9ef3482481f4d9c3e45
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2025-01-16 17:37:29 +0900

    [Gold V] Title: 수 고르기, Time: 924 ms, Memory: 112988 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+        int M = input.nextInt();
+
+        int[] array = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            array[i] = input.nextInt();
+        }
+
+        Arrays.sort(array);
+
+        int start = 0;
+        int end = 0;
+        int answer = Integer.MAX_VALUE;
+
+        while(start < N && end < N) {
+            if(array[end] - array[start] == M) {
+                answer = M;
+                break;
+            }
+            if(array[end] - array[start] > M) {
+                answer = Math.min(answer, array[end] - array[start]);
+                start++;
+            } else {
+                end++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
