78f128f644f38ec9c8f0e1647ad692d2069e4369
Yeahyun Kim <insidesy4@gmail.com>
2025-02-24 18:54:24 +0900

    [2644] 촌수계산

---
+++ backjoonStudy/src/main/java/org/DFS/촌수계산_2644.java
@@ -0,0 +1,60 @@
+package org.DFS;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class 촌수계산_2644 {
+    static int N, A, B, M;
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int depth = 0;
+
+    public static void dfs(int idx, int end, int count) {
+        visited[idx] = true;
+
+        if(idx == end) {
+            depth = count;
+            return;
+        }
+        
+        for(int i = 0; i < graph[idx].size(); i++) {
+            if(!visited[graph[idx].get(i)]) {
+                dfs(graph[idx].get(i), end, count+1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        N = input.nextInt();
+        A = input.nextInt();
+        B = input.nextInt();
+        M = input.nextInt();
+
+        graph = new ArrayList[N+1];
+        visited = new boolean[N+1];
+
+        for(int i = 0; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < M; i++) {
+            int a = input.nextInt();
+            int b = input.nextInt();
+
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        dfs(A, B, 0);
+
+        if(A == B) {
+            System.out.println(0);
+        } else {
+            System.out.println(depth == 0 ? -1 : depth);
+        }
+
+
+    }
+}
