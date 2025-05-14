0e440699f20ba99586a5e4d8696dd1369821b3d9
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2025-05-14 10:27:05 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.21 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = numbers[0] * numbers[1];
+        
+        for(int i = 0; i < numbers.length - 1; i++) {
+            for(int j = i+1; j < numbers.length; j++) {
+                answer = Math.max(numbers[i] * numbers[j], answer);
+            }
+        }
+        return answer;
+    }
+}
