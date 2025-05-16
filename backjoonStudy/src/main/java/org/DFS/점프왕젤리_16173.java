d33bec532eb6ba921596b2ffb9722d9d351ee2a3
YeahyunKim <insidesy4@gmail.com>
2025-05-16 11:28:09 +0900

    [16173] 점프왕 젤리

---
+++ backjoonStudy/src/main/java/org/DFS/점프왕젤리_16173.java
@@ -0,0 +1,56 @@
+package org.DFS;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 점프왕젤리_16173 {
+    static int N;
+    static int[][] graph;
+    static boolean[][] visited;
+    static int[] dirY = {1, 0};
+    static int[] dirX = {0, 1};
+
+    public static void dfs(int y, int x) {
+        visited[y][x] = true;
+        if(y == N-1 && x == N-1) {
+            return;
+        }
+
+        for(int i = 0; i < 2; i++) {
+            int newY = dirY[i] * graph[y][x] + y;
+            int newX = dirX[i] * graph[y][x] + x;
+
+            if(newY < N && newX < N) {
+                if(!visited[newY][newX]) {
+                    dfs(newY, newX);
+                }
+            }
+        }
+    }
+
+
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        N = input.nextInt();
+
+        graph = new int[N][N];
+        visited = new boolean[N][N];
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                int a = input.nextInt();
+                graph[i][j] = a;
+            }
+        }
+
+        dfs(0, 0);
+
+        if(visited[N-1][N-1] == true) {
+            System.out.println("HaruHaru");
+        } else {
+            System.out.println("Hing");
+        }
+
+    }
+}
