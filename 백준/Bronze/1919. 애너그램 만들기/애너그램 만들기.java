9b58a6735796fd042587f0b4b9808024b4bdffe7
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2025-11-11 14:03:51 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 172 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,30 +1,32 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+public class Main{
 
-    public static int[] addAlphabetCount(String s) {
+    public static int[] countStrAsAlphabets(String s) {
         int[] countStr = new int[26];
+        
         for(int i = 0; i < s.length(); i++) {
             countStr[s.charAt(i) - 'a']++;
         }
+        
         return countStr;
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        String str1 = input.next();
-        String str2 = input.next();
-
-        int[] countStr1 = addAlphabetCount(str1);
-        int[] countStr2 = addAlphabetCount(str2);
+        
+        String a = input.nextLine();
+        String b = input.nextLine();
+        
+        int[] countA = countStrAsAlphabets(a);
+        int[] countB = countStrAsAlphabets(b);
 
         int answer = 0;
 
         for(int i = 0; i < 26; i++) {
-            if(countStr1[i] != countStr2[i]) answer += Math.abs(countStr1[i] - countStr2[i]);
+            if(countA[i] != countB[i]) answer += Math.abs(countA[i] - countB[i]);
         }
-
+        
         System.out.println(answer);
-
+        
     }
 }
