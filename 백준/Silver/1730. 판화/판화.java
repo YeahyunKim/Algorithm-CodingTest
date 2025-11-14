dbaab18d15ca29d18d3898ddc55407b9426decf6
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2025-11-14 16:16:09 +0900

    [Silver IV] Title: 판화, Time: 172 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Silver/1730. 판화/판화.java
+++ 백준/Silver/1730. 판화/판화.java
@@ -1,5 +1,4 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,62 +6,57 @@ public class Main {
 
         int N = input.nextInt();
         input.nextLine();
-
-        String[] s = input.nextLine().split("");
-        String[][] map = new String[N][N];
+        
+        char[][] map = new char[N][N];
 
         for(int i = 0; i < N; i++) {
-            Arrays.fill(map[i], ".");
+            for(int j = 0; j < N; j++) {
+                map[i][j] = '.';
+            }
         }
+        
+        String LOCATION = input.nextLine();
 
-        int x = 0;
         int y = 0;
-        for(int i = 0; i < s.length; i++) {
-            int newX = x;
+        int x = 0;
+        for(char l : LOCATION.toCharArray()) {
             int newY = y;
-            String now = s[i];
+            int newX = x;
+            if(l == 'D') newY += 1;
+            else if(l == 'U') newY -= 1;
+            else if(l == 'R') newX += 1;
+            else if(l == 'L') newX -= 1;
+            
+            if(newY < N && newX < N && newY >=0 && newX >= 0) {
+                changeMap(map, l, y, x, newY, newX);   
+                y = newY;
+                x = newX;
+            }
 
-            if(now.equals("D")) newY += 1;
-            else if(now.equals("U")) newY -= 1;
-            else if(now.equals("R")) newX += 1;
-            else if(now.equals("L")) newX -= 1;
-
-            if(newY < 0 || newY >= N || newX < 0 || newX >= N) continue;
-
-            changeMap(map, y, x, newY, newX, now);
-            x = newX;
-            y = newY;
         }
-
-
+        
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
                 System.out.print(map[i][j]);
             }
-            if(i != N-1) {
-                System.out.println();
-            }
+            System.out.println();
         }
-    }
-
-    public static String[][] changeMap(String[][] array, int y1, int x1, int y2, int x2, String direction) {
-        if(direction.equals("D") || direction.equals("U")) {
-            if(array[y1][x1].equals(".")) array[y1][x1] = "|";
-            else if(array[y1][x1].equals("-")) array[y1][x1] = "+";
-
-            if(array[y2][x2].equals(".")) array[y2][x2] = "|";
-            else if(array[y2][x2].equals("-")) array[y2][x2] = "+";
-
+    }   
+    public static void changeMap(char[][] map, char location, int y, int x, int y2, int x2) {
+        if(location == 'D' || location == 'U') {
+            if(map[y][x] == '.') map[y][x] = '|';
+            else if(map[y][x] == '-') map[y][x] = '+';
+            
+            if(map[y2][x2] == '.') map[y2][x2] = '|';
+            else if(map[y2][x2] == '-') map[y2][x2] = '+';
+        }    
+        
+        if(location == 'R' || location == 'L') {
+            if(map[y][x] == '.') map[y][x] = '-';
+            else if(map[y][x] == '|') map[y][x] = '+';
+            
+            if(map[y2][x2] == '.') map[y2][x2] = '-';
+            else if(map[y2][x2] == '|') map[y2][x2] = '+';
         }
-
-        if(direction.equals("R") || direction.equals("L")) {
-            if(array[y1][x1].equals(".")) array[y1][x1] = "-";
-            else if(array[y1][x1].equals("|")) array[y1][x1] = "+";
-
-            if(array[y2][x2].equals(".")) array[y2][x2] = "-";
-            else if(array[y2][x2].equals("|")) array[y2][x2] = "+";
-        }
-
-        return array;
     }
 }
