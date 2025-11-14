df9d7016d0b12f19adb42581a20489c5e769825e
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2025-11-14 16:25:56 +0900

    [Silver V] Title: 단어 정렬, Time: 728 ms, Memory: 61168 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -10,10 +10,11 @@ public class Main {
         for(int i = 0; i < N; i++) {
             hashSet.add(input.nextLine());
         }
-        ArrayList<String> arrayList = new ArrayList<>(hashSet);
         
-        Collections.sort(arrayList, new Comparator<String>() {
-           @Override
+        ArrayList<String> strArray = new ArrayList<>(hashSet);
+        
+        Collections.sort(strArray, new Comparator<String>() {
+            @Override
             public int compare(String o1, String o2) {
                 int s1 = o1.length();
                 int s2 = o2.length();
@@ -24,13 +25,11 @@ public class Main {
                 } else {
                     return o1.compareTo(o2);
                 }
-                
             }
         });
         
-        for(String s : arrayList) {
-            System.out.println(s);
+        for(int i = 0; i < strArray.size(); i++) {
+            System.out.println(strArray.get(i));   
         }
-        
-    }
+    }   
 }
