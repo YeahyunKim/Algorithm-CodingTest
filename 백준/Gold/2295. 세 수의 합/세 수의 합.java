6418a2b15a0c78bbf452c34ee86131fd8ac6790c
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2025-11-20 15:13:49 +0900

    [Gold IV] Title: 세 수의 합, Time: 472 ms, Memory: 67484 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -1,30 +1,38 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-
-            int N = sc.nextInt();
-            int[] arr = new int[N];
-            for (int i = 0; i < N; i++)
-                arr[i] = sc.nextInt();
-
-            Set<Integer> sums = new HashSet<>();
-            for (int i = 0; i < N-1; i++)
-                for (int j = i; j < N; j++)
-                    sums.add(arr[i] + arr[j]);
-
-            int ans = -1;
-            for (int i = 0; i < N; i++)
-                for (int j = 0; j < N; j++) {
-                    int target = arr[i] - arr[j];
-                    if (sums.contains(target))
-                        ans = Math.max(ans, arr[i]);
-                }
-            System.out.println(ans);
+        Scanner input = new Scanner(System.in);
+        
+        int N = input.nextInt();
+        
+        int[] array = new int[N];
+        
+        for(int i = 0; i < N; i++) {
+            int num = input.nextInt();
+            array[i] = num;
         }
-
+        
+        Arrays.sort(array);
+        
+        HashSet<Integer> hashSet = new HashSet<>();
+        
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                hashSet.add(array[i] + array[j]);
+            }
+        }
+        
+        
+        int answer = 0;
+        for(int i = N-1; i > 0 ; i--) {
+            for(int j = i-1; j >= 0; j--) {
+                if(hashSet.contains(array[i]-array[j])) {
+                    answer = Math.max(answer, array[i]);
+                }
+            }
+        }
+        
+        System.out.println(answer);
+    }   
 }
