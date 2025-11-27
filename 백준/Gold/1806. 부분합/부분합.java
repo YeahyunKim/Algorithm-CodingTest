b402199ab0d8ffbf88c0361cfa2d530e5e0f6345
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2025-11-27 11:58:11 +0900

    [Gold IV] Title: 부분합, Time: 708 ms, Memory: 112560 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,37 +1,41 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    static int N, S;
+        
     public static void main(String[] args) {
-               Scanner input = new Scanner(System.in);
-
-        int N = input.nextInt();
-        int M = input.nextInt();
-
-        int[] array = new int[N + 1];
-
+        Scanner input = new Scanner(System.in);     
+        
+        N = input.nextInt();
+        S = input.nextInt();
+        
+        int[] array = new int[N+1];
+        
         for(int i = 0; i < N; i++) {
             array[i] = input.nextInt();
         }
-
-        int start = 0;
-        int end = 0;
-        int answer = Integer.MAX_VALUE;
-        int sum = 0;
         
-        while(start <= N && end <= N) {
-            if(sum >= M && answer > end - start) answer = end - start;
+        int i = 0;
+        int j = 0;
+        
+        int sum = 0;
 
-            if(sum < M) {
-                sum += array[end++];
+        int answer = Integer.MAX_VALUE;
+        
+        while(i <= N && j <= N) {
+            if(sum >= S && answer > j - i) answer = j - i;
+            
+            if(sum < S) {
+                sum += array[j++];
             } else {
-                sum -= array[start++];
-
+                sum -= array[i++];
             }
+            
         }
-        if (answer == Integer.MAX_VALUE) {
-            System.out.println(0);
-        } else {
-            System.out.println(answer);
+        
+        if(answer == Integer.MAX_VALUE) {
+            answer = 0;
         }
+        System.out.print(answer);
     }
 }
