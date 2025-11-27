53a968e6e8fb2e9f18334db9a537a9980e1c0d32
Yeahyun Kim <132278619+YeahyunKim@users.noreply.github.com>
2025-11-27 13:30:56 +0900

    [Gold V] Title: 수 고르기, Time: 920 ms, Memory: 109688 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -1,37 +1,54 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        
         int N = input.nextInt();
         int M = input.nextInt();
-
+        
         int[] array = new int[N];
-
+        
         for(int i = 0; i < N; i++) {
             array[i] = input.nextInt();
         }
-
+        
         Arrays.sort(array);
-
-        int start = 0;
-        int end = 0;
+        
+        int i = 0;
+        int j = 0;
         int answer = Integer.MAX_VALUE;
-
-        while(start < N && end < N) {
-            if(array[end] - array[start] == M) {
-                answer = M;
-                break;
-            }
-            if(array[end] - array[start] > M) {
-                answer = Math.min(answer, array[end] - array[start]);
-                start++;
+        
+        // 1 2 3 4 5
+        // 차이가 3 이상일 때,
+        // (1, 4) , (1, 5) 정답은 3;
+        
+        // i = 0   [1] 2 3 4 5
+        // j = 1    1 [2] 3 4 5
+        // 차이가 < 3 ==> j++
+        
+        // i = 0   [1] 2 3 4 5
+        // j = 2    1 2 [3] 4 5
+        // 차이가 < 3 ==> j++
+        
+        // i = 0   [1] 2 3 4 5
+        // j = 3    1 2 3 [4] 5
+        // 차이가 <= 3 ==> 정답
+        
+        
+        // 1 5 10 11 12
+        
+        while(i < N && j < N) {
+            if(array[j] - array[i] >= M && answer > array[j] - array[i]) answer = array[j] - array[i];
+            
+            if(array[j] - array[i] > M) {
+                i++;
             } else {
-                end++;
+                j++;
             }
         }
-        System.out.println(answer);
+        
+        System.out.print(answer);
+       
     }
 }
